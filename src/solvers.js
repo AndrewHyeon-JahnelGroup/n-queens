7a568e210bdced3a92d3ba9bfa5eec902c20bb71
Hack Reactor Student <student@hackreactor.com>
2017-07-13 18:43:40 -0700

    Finished counts N queens

--- src/solvers.js
+++ src/solvers.js
@@ -57,8 +57,35 @@ window.findNQueensSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
-
+  var solutionCount = 0; //fixme
+  var board = new Board ({n});
+  
+  if (n === 2 || n === 3) {
+    return solutionCount;
+  }
+  if (n === 1) {
+    return 1;
+  }
+  
+  var solutions = function(row) {
+    
+    if (row === n) {
+      solutionCount++;
+      return;
+    }
+    
+    for (var i = 0; i < n; i++) {
+      
+      board.togglePiece(row, i);
+      if (!board.hasAnyQueensConflicts()) {
+        solutions(row + 1);  
+      }
+      board.togglePiece(row, i);
+    }
+    
+  }; 
+  
+  solutions(0);
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
